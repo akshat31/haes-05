47533301046b8890e8637075ce55a14f7dba9fd6
Akshat Soni <akshat.soni31@gmail.com>
2026-07-23 23:56:43 +0200

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import './index.css'
 const root = createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="haes-05">
       <App />
     </BrowserRouter>
   </React.StrictMode>
